e7491191840abcd4bc4488506075bacf5c864d71
yuqih <yuki.h@sjtu.edu.cn>
2022-03-09 13:14:03 +0800

    case 1

--- src/exchange.cpp
+++ src/exchange.cpp
@@ -6,6 +6,13 @@
 
 #define log(fmt, ...) printf("[%s:%d] " fmt, __FILE__, __LINE__, ## __VA_ARGS__)
 
+#define EXCHANGE_DEBUG
+#ifdef EXCHANGE_DEBUG
+    #define ex_debug log
+#else
+    #define ex_debug(fmt, ...) do {} while (0)
+#endif
+
 /* TODO: move to .h */
 struct Order {
     int stk_code;
@@ -27,7 +34,7 @@ struct Trade {
 struct Record {
     int order_id;
     double price;
-    int volumn;
+    int volume;
 };
 
 struct BuyRecord : public Record {
@@ -42,7 +49,28 @@ struct SellRecord : public Record {
     };
 };
 
+/* for debugging */
+void printRecord(Record& r) {
+    printf("order_id:%d\tprice:%.2f\tvolume:%d\n", r.order_id, r.price, r.volume);
+}
+
+void printRecordList(std::vector<Record>& rv) {
+    for (auto& r: rv) {
+        printRecord(r);
+    }
+}
+
 class StockDeclarationBook {
+/**
+ * @brief 集中申报簿
+ *
+ * `buy_decls` 按照 price 降序（买价越高越优先）
+ * `sell_decls` 按照 price 升序（卖价越低越优先）
+ * 两者均可能为空，当两者均不为空时，买一价 严格小于 卖一价
+ *
+ * NOTE: 应当注意 price 的排序是不严格的，相邻两个 Record 的 price 可能相同
+ *
+ */
 private:
     std::vector<BuyRecord> buy_decls;
     std::vector<SellRecord> sell_decls;
@@ -67,10 +95,20 @@ public:
         return &buy_decls[0];
     }
 
+    void removeBuyFirst() {
+        assert(!buy_decls.empty());
+        buy_decls.erase(buy_decls.begin());
+    }
+
     SellRecord* querySellFirst() {
         if (sell_decls.empty())
             return nullptr;
         return &sell_decls[0];
+    }
+
+    void removeSellFirst() {
+        assert(!sell_decls.empty());
+        sell_decls.erase(sell_decls.begin());
     }
 };
 
@@ -127,6 +165,9 @@ public:
             /* Erase commited order */
             std::pop_heap(not_ready_orders.begin(), not_ready_orders.end(), orderGtById);
             not_ready_orders.pop_back();
+
+            /* Increase last_commit_id */
+            last_commit_order_id++;
         }
 
         return ret;
@@ -137,16 +178,68 @@ public:
          * 1. split by order type
          * 2. handle for each type, and return status
          */
-        return -1;
 
         switch (order.type) {
             case 0: {
                 /* TODO: 限价申报 */
                 break;
             }
-            case 1: {
-                /* 对手方最优价格申报，以申报进入交易主机时集中申报簿中对手方队列的最优价格为其申报价格。*/
+            case 1: { /* 对手方最优价格申报 */
 
+                /* 以申报进入交易主机时集中申报簿中对手方队列的最优价格为其申报价格。*/
+                if (order.direction == 1) {
+                    /* Buy in */
+                    SellRecord* sr = decl_book.querySellFirst();
+
+                    /* 集中申报簿中，对手方（卖方）无申报：直接撤回此申报 */
+                    if (sr == nullptr)
+                        return -1;
+
+                    double t_price = sr->price;
+                    int left_volume = order.volume;
+
+                    /* NOTE: 可能需要比对多组价格相同的 SellRecord */
+                    while (left_volume) {
+                        if (left_volume < sr->volume) {
+                            /* 当前申报可以完全处理，卖一还有剩余 */
+                            Trade new_trade;
+                            new_trade.stk_code = stk_code;
+                            new_trade.bid_id = order.order_id; /* buyer's order_id */
+                            new_trade.ask_id = sr->order_id; /* seller's order_id */
+                            new_trade.price = t_price;
+                            new_trade.volume = left_volume;
+                            trade_list.push_back(new_trade);
+
+                            left_volume -= left_volume;
+                            sr->volume -= left_volume;
+                        } else {
+                            /* 卖一将被完全处理，当前申报继续 */
+                            Trade new_trade;
+                            new_trade.stk_code = stk_code;
+                            new_trade.bid_id = order.order_id;
+                            new_trade.ask_id = sr->order_id;
+                            new_trade.price = t_price;
+                            new_trade.volume = sr->volume;
+                            trade_list.push_back(new_trade);
+
+                            left_volume -= sr->volume;
+                            decl_book.removeSellFirst();
+
+                            sr = decl_book.querySellFirst();
+                            if (sr == nullptr || sr->price != t_price) {
+                                /* 卖一价格不足交易，将剩余部分以限价形式记录在集中申报簿中 */
+                                BuyRecord new_br;
+                                new_br.price = t_price;
+                                new_br.volume = left_volume;
+                                decl_book.insertBuyDecl(new_br);
+
+                                left_volume -= left_volume;
+                            }
+                        }
+                    }
+
+
+                }
                 break;
             }
             case 2: {
@@ -166,6 +259,8 @@ public:
                 break;
             }
         }
+
+        return 0;
     }
 };
 

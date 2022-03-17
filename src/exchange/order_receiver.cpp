6289a27d01cc78eecca99014d0fab56e6965c167
Yusen Xu <xuyusen@sjtu.edu.cn>
2022-03-17 16:36:38 +0800

    remove debug output; add thpt monitor

--- src/exchange/order_receiver.cpp
+++ src/exchange/order_receiver.cpp
@@ -1,4 +1,6 @@
 #include "order_receiver.h"
+
+#include "common/monitor.hpp"
 #include "exchange.h"
 
 namespace ubiquant {
@@ -6,7 +8,7 @@ namespace ubiquant {
 ExchangeOrderReceiver::ExchangeOrderReceiver() {
     // init msg receivers
     std::vector<int> ports;
-    for(int i = 0; i < Config::trader_num; i++) {
+    for (int i = 0; i < Config::trader_num; i++) {
         ports.push_back(Config::trader_port2exchange_port[i][Config::partition_idx][0].second);
         ports.push_back(Config::trader_port2exchange_port[i][Config::partition_idx][1].second);
     }
@@ -15,7 +17,9 @@ ExchangeOrderReceiver::ExchangeOrderReceiver() {
 
 void ExchangeOrderReceiver::run() {
     logstream(LOG_EMPH) << "Exchange OrderReceiver is running..." << LOG_endl;
-    while(true) {
+    Monitor monitor;
+    monitor.start_thpt();
+    while (true) {
         std::string msg = msg_receiver_->recv();
         // deserialize orders
         size_t offset = 0;
@@ -25,12 +29,14 @@ void ExchangeOrderReceiver::run() {
         uint32_t cnt = 0;
         get_elem_from_buf(msg.c_str(), offset, cnt);
         Order order;
-        for(uint32_t i = 0; i < cnt; i++) {
+        for (uint32_t i = 0; i < cnt; i++) {
             get_elem_from_buf(msg.c_str(), offset, order);
             Global<Exchange>::Get()->receiveOrder(order);
+            monitor.add_cnt();
         }
+        monitor.print_timely_thpt("Order Receiver Throughput");
     }
+    monitor.end_thpt();
 }
 
 }  // namespace ubiquant
-
--- src/trader/trade_receiver.cpp
+++ src/trader/trade_receiver.cpp
@@ -1,6 +1,7 @@
-#include "common/global.hpp"
-
 #include "trade_receiver.h"
+
+#include "common/global.hpp"
+#include "common/monitor.hpp"
 #include "trader_controller.h"
 
 namespace ubiquant {
@@ -9,12 +10,12 @@ TraderTradeReceiver::TraderTradeReceiver() {
     // init stock code (ALL)
     std::vector<int> stk_codes;
     // NOTICE: stock code starts from 1
-    for(int i = 1; i <= Config::stock_num; i++) {
+    for (int i = 1; i <= Config::stock_num; i++) {
         stk_codes.push_back(i);
     }
 
     ASSERT(!Config::trade_output_folder.empty());
-    for(auto code : stk_codes) {
+    for (auto code : stk_codes) {
         std::string path = Config::trade_output_folder + "/trade_res." + std::to_string(code);
         trade_fds_[code] = open(path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0640);
         if (trade_fds_[code] == EMPTY_FD)
@@ -31,50 +32,53 @@ TraderTradeReceiver::TraderTradeReceiver() {
 
     // init msg receivers
     std::vector<int> ports;
-    for(int i = 0; i < Config::exchange_num; i++) {
+    for (int i = 0; i < Config::exchange_num; i++) {
         ports.push_back(Config::trader_port2exchange_port[Config::partition_idx][i][2].first);
     }
     msg_receiver_ = std::make_shared<MessageReceiver>(ports);
 }
 
-TraderTradeReceiver::~TraderTradeReceiver()
-{
+TraderTradeReceiver::~TraderTradeReceiver() {
     // flush all trades
     flush();
     // close fd
-    for(auto [stk_code, fd] : trade_fds_) {
+    for (auto [stk_code, fd] : trade_fds_) {
         if (fd != EMPTY_FD) close(fd);
     }
 }
 
 void TraderTradeReceiver::run() {
-    while(!Global<TraderController>::Get() || !Global<TraderController>::Get()->is_inited()) {
+    while (!Global<TraderController>::Get() || !Global<TraderController>::Get()->is_inited()) {
         usleep(1);
     }
     logstream(LOG_EMPH) << "Trader TradeReceiver is running..." << LOG_endl;
-    while(true) {
+
+    monitor.start_thpt();
+    while (true) {
         std::string msg = msg_receiver_->recv();
-        if(msg.size() == 32) {
-            double price = 0;
-            size_t off = 24;
-            get_elem_from_buf(msg.c_str(), off, price);
-            std::cout << "before serial trade:" << msg.size()  << ", price:" << price << std::endl;
-        }
+        // if (msg.size() == 32) {
+        //     double price = 0;
+        //     size_t off = 24;
+        //     get_elem_from_buf(msg.c_str(), off, price);
+        //     std::cout << "before serial trade:" << msg.size() << ", price:" << price << std::endl;
+        // }
         uint32_t msg_code;
         size_t offset = 0;
         get_elem_from_buf(msg.c_str(), offset, msg_code);
-        if(msg_code == MSG_TYPE::ORDER_ACK_MSG) {
+        if (msg_code == MSG_TYPE::ORDER_ACK_MSG) {
             process_order_ack(msg);
         } else if (msg_code == MSG_TYPE::TRADE_MSG) {
             process_trade_result(msg);
         } else {
             ASSERT_MSG(false, "Wrong message code!");
         }
+        monitor.print_timely_thpt("Trade Receiver Throughput");
     }
+    monitor.end_thpt();
 }
 
 Trade convert_commtrade_to_trade(const CommTrade& commTrade) {
-    return Trade {
+    return Trade{
         stk_code : commTrade.stk_code,
         bid_id : commTrade.bid_id,
         ask_id : commTrade.ask_id,
@@ -90,29 +94,29 @@ void TraderTradeReceiver::process_trade_result(std::string& msg) {
     // get_elem_from_buf(msg.c_str(), off, price);
     // std::cout << "before serial trade:" << msg.size()  << ", price:" << price << std::endl;
     std::vector<Trade> trades;
-    size_t offset = sizeof(uint32_t); // skip msg code
+    size_t offset = sizeof(uint32_t);  // skip msg code
     uint32_t cnt = 0;
     get_elem_from_buf(msg.c_str(), offset, cnt);
     trades.resize(cnt);
-    for(auto& trade : trades) {
+    for (auto& trade : trades) {
         CommTrade commTrade;
         get_elem_from_buf(msg.c_str(), offset, commTrade);
         trade = convert_commtrade_to_trade(commTrade);
+        monitor.add_cnt();
     }
-    
-    for(auto& trade : trades) {
-        trade.print();
+
+    for (auto& trade : trades) {
+        // trade.print();
         // update hook in controller
         Global<TraderController>::Get()->update_if_hooked(trade.stk_code, trade_idxs_[trade.stk_code], trade.volume);
         trade_idxs_[trade.stk_code]++;
 
         trade_buffer_[trade.stk_code].push_back(trade);
         // batch write
-        if(trade_buffer_[trade.stk_code].size() >= TRADE_BUF_THRESHOLD) {
-            if ((size_t)write(trade_fds_[trade.stk_code], 
-                              trade_buffer_[trade.stk_code].data(), 
-                              trade_buffer_[trade.stk_code].size() * sizeof(Trade)) 
-                    != trade_buffer_[trade.stk_code].size() * sizeof(Trade)) {
+        if (trade_buffer_[trade.stk_code].size() >= TRADE_BUF_THRESHOLD) {
+            if ((size_t)write(trade_fds_[trade.stk_code],
+                              trade_buffer_[trade.stk_code].data(),
+                              trade_buffer_[trade.stk_code].size() * sizeof(Trade)) != trade_buffer_[trade.stk_code].size() * sizeof(Trade)) {
                 throw std::runtime_error("write trade file error.");
             }
 
@@ -125,12 +129,11 @@ void TraderTradeReceiver::process_trade_result(std::string& msg) {
 }
 
 void TraderTradeReceiver::flush() {
-    for(auto& [stk_code, buffer] : trade_buffer_) {
+    for (auto& [stk_code, buffer] : trade_buffer_) {
         // batch write
-        if ((size_t)write(trade_fds_[stk_code], 
-                          buffer.data(), 
-                          buffer.size() * sizeof(Trade)) 
-                != buffer.size() * sizeof(Trade)) {
+        if ((size_t)write(trade_fds_[stk_code],
+                          buffer.data(),
+                          buffer.size() * sizeof(Trade)) != buffer.size() * sizeof(Trade)) {
             throw std::runtime_error("write trade file error.");
         }
 
@@ -144,19 +147,18 @@ void TraderTradeReceiver::flush() {
 void TraderTradeReceiver::process_order_ack(std::string& msg) {
     // de-serialize
     std::vector<OrderAck> acks;
-    size_t offset = sizeof(uint32_t); // skip msg code
+    size_t offset = sizeof(uint32_t);  // skip msg code
     uint32_t cnt = 0;
     get_elem_from_buf(msg.c_str(), offset, cnt);
     acks.resize(cnt);
-    for(auto& ack : acks) {
+    for (auto& ack : acks) {
         get_elem_from_buf(msg.c_str(), offset, ack);
     }
 
     // update sliding window start in controller
-    for(auto& ack : acks) {
-        Global<TraderController>::Get()->update_sliding_window_start(ack.stk_code, ack.order_id+1);
+    for (auto& ack : acks) {
+        Global<TraderController>::Get()->update_sliding_window_start(ack.stk_code, ack.order_id + 1);
     }
 }
 
 }  // namespace ubiquant
-
